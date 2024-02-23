ce13c8d3be76a2fe49775e6c9a4d39f01944421f
estlopper <estlopper@alum.us.es>
2024-02-23 12:26:59 +0100

    commit 1

--- DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
+++ DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
@@ -6,12 +6,90 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      address: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      postalCode: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      url: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      restaurantCategoryId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'RestaurantCategories'
+          },
+          key: 'id'
+        }
+      },
+      shippingCosts: {
+        allowNull: false,
+        type: Sequelize.DOUBLE
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true
+      },
+      logo: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      phone: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'Users'
+          },
+          key: 'id'
+        }
+      },
+      status: {
+        type: Sequelize.ENUM,
+        values: ['online', 'offline', 'closed', 'temporally closed']
+      },
+      heroImage: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      averageServiceMinutes: {
+        allowNull: true,
+        type: Sequelize.DOUBLE
       }
-      // TODO: Include the rest of the fields of the Restaurants table
-
+      // quedan por añadir las foreign key
     })
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('Restaurants')
   }
+
 }
